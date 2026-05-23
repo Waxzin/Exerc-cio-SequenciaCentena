fd0a75e16bdb4d1b2d49d209f301cef1f0bb93bf
matheus Luan <matheusluan04@gmail.com>
2026-05-22 21:39:42 -0300

    Conclusão de exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Exibindo os 100 primeiros numeros ");
+int contador = 0;
+int maximo = 100;
+
+while(contador <= maximo)
+{
+    Console.Write(contador);
+    contador = contador + 1;
+    
+}
+
+Console.WriteLine("");
+ Console.WriteLine(" Voce chegou no maximo ");
